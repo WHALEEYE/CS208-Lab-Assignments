3d49a464a71c172f474efc731d800a188aa94663
WHALEEYE <1094349973@qq.com>
2021-03-12 23:13:04 +0800

    lab01 finished

--- Lab00.cpp
+++ Lab00.cpp
@@ -6,16 +6,21 @@ int main() {
   cin.tie(0);
   cout.tie(0);
   int t, n, l;
+  long long sum;
   cin >> t;
   while (t--) {
+    sum = 0;
     cin >> n >> l;
     long long *fib = new long long[n + 1]();
     fib[0] = 1;
     for (int i = 1; i <= n; i++) {
-      int k = l < i ? l : i;
-      for (int j = 1; j <= k; j++) {
-        fib[i] += fib[i - j];
+      if (i <= l) {
+        sum += fib[i - 1];
+      } else {
+        sum += fib[i - 1];
+        sum -= fib[i - l - 1];
       }
+      fib[i] = sum;
       fib[i] %= 998244353;
     }
     cout << fib[n] << '\n';
