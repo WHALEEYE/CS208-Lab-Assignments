0744a2fc8a5ff38189e75154c64b571bbe9f4368
WHALEEYE <1094349973@qq.com>
2021-03-13 00:05:32 +0800

    Update Lab02.cpp

--- Lab02.cpp
+++ Lab02.cpp
@@ -2,18 +2,18 @@
 #include <iostream>
 using namespace std;
 
-bool compare(int a, int b) { return a > b; }
+bool compare(long long a, long long b) { return a > b; }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
+  // ios::sync_with_stdio(false);
+  // cin.tie(0);
+  // cout.tie(0);
   long long t, n, a, s;
   long long max, sum1, sum2, temp1, temp2;
-  bool alr1, alr2;
+  int alr1, alr2;
   cin >> t;
   while (t--) {
-    alr1 = alr2 = false;
+    alr1 = alr2 = 0;
     sum1 = sum2 = max = temp1 = temp2 = 0;
     s = 0;
     cin >> n;
@@ -32,7 +32,7 @@ int main() {
     sort(ipt, ipt + s, compare);
     if (s == 1) {
       cout << ipt[0] << '\n';
-      return 0;
+      continue;
     }
     if (ipt[0] % ipt[1] != 0) {
       max = ipt[0] + ipt[1];
@@ -47,39 +47,44 @@ int main() {
       sum1 = ipt[0];
       sum2 = ipt[1];
       for (int i = 2; i < s; i++) {
-        if (ipt[0] % ipt[i] != 0 && !alr1) {
-          alr1 = true;
+        if (ipt[0] % ipt[i] != 0 && (alr1 == 0)) {
+          alr1 = 1;
           sum1 += ipt[i];
           temp1 = ipt[i];
-        } else if (ipt[1] % ipt[i] != 0 && !alr2) {
-          alr2 = true;
+        } else if ((ipt[0] % ipt[i] != 0) && (temp1 % ipt[i] != 0) &&
+                   (alr1 == 1)) {
+          alr1 = 2;
+          sum1 += ipt[i];
+        }
+        if ((ipt[1] % ipt[i] != 0) && (alr2 == 0)) {
+          alr2 = 1;
           sum2 += ipt[i];
           temp2 = ipt[i];
-        } else if (ipt[0] % ipt[i] != 0 && temp1 % ipt[i] != 0 && alr1) {
-          sum1 += ipt[i];
-          break;
-        } else if (ipt[1] % ipt[i] != 0 && temp2 % ipt[i] != 0 && alr2) {
+        } else if ((ipt[1] % ipt[i] != 0) && (temp2 % ipt[i] != 0) &&
+                   (alr2 == 1)) {
+          alr2 = 2;
           sum2 += ipt[i];
+        }
+        if (alr1 == 2 && alr2 == 2) {
           break;
         }
       }
       max = sum1 > sum2 ? sum1 : sum2;
       cout << max << '\n';
-    } else if (ipt[0] % ipt[1] == 0 && ipt[0] / ipt[1] >= 3) {
+    } else if ((ipt[0] % ipt[1] == 0) && (ipt[0] / ipt[1] >= 3)) {
       max = ipt[0];
       for (int i = 2; i < s; i++) {
-        if (ipt[0] % ipt[i] != 0 && !alr1) {
-          alr1 = true;
+        if ((ipt[0] % ipt[i] != 0) && (alr1 == 0)) {
+          alr1 = 1;
           max += ipt[i];
           temp1 = ipt[i];
-        } else if (ipt[0] % ipt[i] != 0 && temp1 % ipt[i] != 0 && alr1) {
+        } else if ((ipt[0] % ipt[i] != 0) && (temp1 % ipt[i] != 0) &&
+                   (alr1 == 1)) {
           max += ipt[i];
           break;
         }
       }
       cout << max << '\n';
     }
-    delete[] ipt;
-    delete[] dup;
   }
 }
